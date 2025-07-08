b5be8728b9bb8bb57736e57e13146456d0633557
Gaigerov <gaigerov@gmail.com>
2025-07-08 14:39:33 +0300

    фикс проблемы с прокси

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import type {FactType} from '../utils/types';
 
+const API_BASE = import.meta.env.PROD 
+  ? 'https://numbersapi.com' 
+  : '/api';
+
 export const fetchFact = async (number: string, type: FactType): Promise<string> => {
   try {
-    const url = type === 'date' 
-      ? `https://cors-anywhere.herokuapp.com/http://numbersapi.com/${number}/${type}?json` 
-      : `https://cors-anywhere.herokuapp.com/http://numbersapi.com/${number}/${type}?json`;
-    
+    const url = `${API_BASE}/${number}/${type}?json`;
     const response = await axios.get(url);
     return response.data.text;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
