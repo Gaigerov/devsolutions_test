1b6c67ee764e21f6e5dd989be3e2cb13037fa9c3
Gaigerov <gaigerov@gmail.com>
2025-07-08 14:58:53 +0300

    правки отображения ответов на  ghpages

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import type {FactType} from '../utils/types';
 
+const API_BASE = import.meta.env.PROD 
+  ? 'https://numbersapi.com' 
+  : '/api';
+
 export const fetchFact = async (number: string, type: FactType): Promise<string> => {
   try {
-    const url = type === 'date' 
-      ? `/api/${number}/${type}?json` 
-      : `/api/${number}/${type}?json`;
-    
+    const url = `${API_BASE}/${number}/${type}?json`;
     const response = await axios.get(url);
     return response.data.text;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
